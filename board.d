73dc1c9a0b5f56c965e51601bc11e1d208e75340
Lee Avital <leeavital@gmail.com>
2013-05-18 19:09:23 -0400

    Added some docs

--- board.d
+++ board.d
@@ -3,11 +3,16 @@ module Board;
 import std.stdio;
 
 
+/**
+ * Represents a scrabble board
+ */
 class Board{
 
    char[26][26] board;
 
    this(){
+
+	  // fill up the board with blank spaces
 	  for( auto i = 0; i < 26; i++){
 		 for( auto j = 0; j < 26; j++){
 			board[i][j] = ' ';
@@ -15,12 +20,18 @@ class Board{
 	  }
    
    }
+   
 
+   /**
+    * allow a board to be accessed with the index operator
+	*/
    char opIndex( int i, int j ){
 	  return board[i][j];
    }
 
-
+   /**
+    * allow the board to be index assigned
+	*/
    void opIndexAssign( char c, int i, int j){
 	  writefln("assigning %d %d as %c", i, j, c );
 	  board[i][j] = c;
